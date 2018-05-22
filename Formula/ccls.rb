c827056cc00c3d919cfcbcc468f58bad10ce0c2a
Gong Qijian <gongqijian@gmail.com>
2018-05-22 20:31:44 +0800

    0.20180521

--- Formula/ccls.rb
+++ Formula/ccls.rb
@@ -1,8 +1,8 @@
 class Ccls < Formula
   desc "A C/C++ language server"
   homepage "https://github.com/MaskRay/ccls"
+  url "https://github.com/MaskRay/ccls.git", :tag => "0.20180521", :revision => "32bde07df64cd6958e19c100c25bab1c1a36f6b7"
   head "https://github.com/MaskRay/ccls.git"
-  url "https://github.com/MaskRay/ccls.git", :tag => "0.20180513", :revision => "89af70fea544a0f52ddbebed282aba1223b2b196"
 
   option "with-build-debug", "Configures ccls to be built in debug mode"
   option "without-system-clang", "Downloading Clang from http://releases.llvm.org/ during the configure process"
@@ -24,10 +24,6 @@ class Ccls < Formula
 
     ENV.prepend_path "PATH", Formula["llvm"].opt_bin
     ENV.prepend_path "PATH", Formula["cmake"].opt_bin
-
-    if build.stable?
-      ENV.append "LDFLAGS", "-lclangBasic"
-    end
 
     system "mkdir", "-p", "#{build_type}"
     system "cmake", *args, "-B#{build_type}", "-H."
