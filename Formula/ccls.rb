0280119edca956a05cc856e826ef5dea6bc0ded6
Birger J. Nordølum <contact@mindtooth.no>
2019-02-21 22:44:01 +0100

    ccls 0.20181225.8

--- Formula/ccls.rb
+++ Formula/ccls.rb
@@ -7,8 +7,8 @@ class Ccls < Formula
         :revision => "06aa25233567ae16a58a6f5237aacfd9e02aa29d"
   else
     url "https://github.com/MaskRay/ccls.git",
-        :tag => "0.20180924",
-        :revision => "41fcc0272c8c93fcbe52f24ef587e633a9dcc7e1"
+        :tag => "0.20181225.8",
+        :revision => "d275ed570d7ea26dd8d762b8d82bc4e295971154"
   end
   head "https://github.com/MaskRay/ccls.git"
 
